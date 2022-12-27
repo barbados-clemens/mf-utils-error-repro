016169d82c633a4ef8e790cb6ad390d453b1c95c
Caleb Ukle <caleb.ukle+github@pm.me>
2022-12-27 15:46:10 -0600

    repro

---
+++ apps/checkout/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mf-utils-error-checkout-entry',
+  template: `<mf-utils-error-nx-welcome></mf-utils-error-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
